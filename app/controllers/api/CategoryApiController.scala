5fe4267f41fa7c89551798ba5d5a29d9571ae1dd
kim.taehune <kim.taehune@nextbeat.net>
2026-06-19 16:25:07 +0900

    カテゴリ作成APIを実装 (機能6: Categoryの追加)
    POST /api/categories と GET /api/colors を追加。
    MPAのCategoryFormをバインドして検証を再利用し、201/400 を返す。

--- app/controllers/api/CategoryApiController.scala
+++ app/controllers/api/CategoryApiController.scala
@@ -7,18 +7,22 @@
 package controllers.api
 
 import javax.inject._
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ ExecutionContext, Future }
 import play.api.mvc._
-import play.api.libs.json.Json
+import play.api.libs.json.{ Json, JsValue }
+import play.api.data.Form
+import play.api.i18n.{ I18nSupport, Messages }
 
 import lib.persistence.CategoryRepository
+import lib.model.Category
+import forms.CategoryForm
 import json.ApiWrites._
 
 @Singleton
 class CategoryApiController @Inject() (
   val controllerComponents: ControllerComponents,
   categoryRepository:       CategoryRepository,
-)(implicit ec: ExecutionContext) extends BaseController {
+)(implicit ec: ExecutionContext) extends BaseController with I18nSupport {
 
   // GET /api/categories : 全カテゴリーを JSON 返却 (Todo 追加フォームの選択肢などに使う)
   def list() = Action.async { implicit req =>
@@ -26,4 +30,39 @@ class CategoryApiController @Inject() (
       Ok(Json.toJson(categories))
     }
   }
+
+  // GET /api/colors : カラーの選択肢 (Category.Color を単一の出典として返す)
+  def colors() = Action {
+    Ok(Json.toJson(Category.Color.values))
+  }
+
+  // POST /api/categories : JSON 本文を検証して作成。201 で作成された Category を返す
+  def create() = Action.async(parse.json) { implicit req =>
+    // MPA と同じ CategoryForm を JSON にバインドして検証 (slug は英数字のみ等)
+    CategoryForm.form.bind(req.body).fold(
+      formWithErrors =>
+        Future.successful(BadRequest(errorsJson(formWithErrors))),
+      data => {
+        val category = Category(
+          id    = None,
+          name  = data.name,
+          slug  = data.slug,
+          color = Category.Color(data.color),
+        )
+        for {
+          newId    <- categoryRepository.add(category.toWithNoId)
+          savedOpt <- categoryRepository.get(newId)
+        } yield savedOpt match {
+          case Some(saved) => Created(Json.toJson(saved))
+          case None        => InternalServerError(Json.obj("message" -> "作成後の取得に失敗しました"))
+        }
+      }
+    )
+  }
+
+  // フォームエラーを { errors: [{ key, message }] } の JSON にする
+  private def errorsJson(form: Form[_])(implicit messages: Messages): JsValue =
+    Json.obj(
+      "errors" -> form.errors.map(e => Json.obj("key" -> e.key, "message" -> messages(e.message, e.args: _*))),
+    )
 }
